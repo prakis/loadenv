730ea955beae2970682f7577fae6c94f478ad62e
Kishore <prakis@gmail.com>
2024-02-07 21:12:11 +0530

    First pre-beta version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prakis/loadenv
+
+go 1.20
---
+++ loadenv.go
@@ -0,0 +1,83 @@
+package loadenv
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+func Test(name string) string {
+
+	message := "Test by " + name
+
+	return message
+}
+
+func Load(envfiles ...string) error {
+
+	envfiles = defaultCheck(envfiles)
+
+	for _, envfilename := range envfiles {
+		err := loadSingeEnvFile(envfilename)
+
+		if err != nil {
+			println(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func defaultCheck(envfiles []string) []string {
+	if len(envfiles) == 0 {
+		return []string{".env"}
+	}
+
+	return envfiles
+}
+
+func loadSingeEnvFile(singleEnvFile string) error {
+
+	envMap := make(map[string]string)
+
+	if len(singleEnvFile) == 0 {
+		return nil
+	}
+	file, err := os.Open(singleEnvFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		// check if the line has = sign
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				// assign the map
+				envMap[key] = value
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	// load this map to os env
+	for envKey, envVal := range envMap {
+		_ = os.Setenv(envKey, envVal)
+	}
+
+	return nil
+}
